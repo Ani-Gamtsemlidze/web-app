e79e97122f34891d55252167cd6a484496819172
User-s <ani.gamcemlidze20@gmail.com>
2023-06-05 19:11:35 +0400

    email-validation-upload

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ const repeatText = document.querySelector(".psw-repeat");
 const pswText = document.querySelector(".psw-input");
 const repeatPsw = document.querySelector(".repeat-psw");
 
+const email = document.querySelector("#signup-email");
+
+const validateEmail = (email) => {
+  var regex =
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(String(email).toLowerCase());
+};
+
 loginButton.addEventListener("click", function (event) {
   if (inputText.value === "") {
     empty.style.display = "block";
@@ -33,5 +41,18 @@ loginButton.addEventListener("click", function (event) {
     event.preventDefault();
   } else {
     document.querySelector(".match").style.display = "none";
+  }
+
+  if (!validateEmail(email.value)) {
+    event.preventDefault();
+
+    empty.style.display = "block";
+
+    document.querySelector(".valid-email").textContent =
+      "It's not a valid email";
+  }
+  if (inputText.value === "") {
+    document.querySelector(".valid-email").textContent = "Can't be empty";
+    event.preventDefault();
   }
 });
